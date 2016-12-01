9f4210992cb94d78f18111613b93d1c0652d07e5
wangxiaoshu <wangxiaoshu@yidian-inc.com>
2016-12-01 14:47:09 +0800

    update: del index initial

--- src/worker.h
+++ src/worker.h
@@ -90,38 +90,65 @@ class Worker : public ps::App{
                 int idx = init_index[i];
                 float g = gradient[idx];
                 if(g != 0.0){
-                        nonzero_index.push_back(idx);
-                        nonzero_gradient.push_back(g);
+                    nonzero_index.push_back(idx);
+                    nonzero_gradient.push_back(g);
                 }
             }
         }
 
-        void calculate_batch_gradient(int& start, int& end, std::vector<float> &w_all){
-            long int idx = 0; int value = 0; float pctr = 0;
-            std::vector<float> g(init_index.size());
+        //void calculate_batch_gradient(int& start, int& end, std::vector<float> &w_all){
+        void calculate_batch_gradient(int& start, int& end){
+            size_t idx = 0; int value = 0; float pctr = 0;
+            std::vector<ps::Key> keys;
+            std::vector<float> w;
             for(int row = start; row < end; ++row){
-                std::vector<ps::Key> keys;
-                std::vector<int> values;
                 for(int j = 0; j < train_data->fea_matrix[row].size(); ++j){//for one instance
                     idx = train_data->fea_matrix[row][j].fid;
                     keys.push_back(idx);
-                    value = train_data->fea_matrix[row][j].val;
-                    values.push_back(value);
                 }
+            }
+            std::sort(keys.begin(), keys.end());
+            kv_.Wait(kv_.Pull(keys, &w));
+
+            std::map<size_t, float> weight;
+            for(int i = 0; i < keys.size(); i++){
+                weight.insert(std::pair<size_t, float>(keys[i], w[i]));
+            }
+
+            std::map<size_t, float> gradient;
+
+            std::map<size_t, float>::iterator iter;
+
+            for(int row = start; row < end; ++row){
                 float wx = bias;
-                for(int j = 0; j < keys.size(); j++){
-                    wx += w_all[keys[j]] * values[j];
+                for(int j = 0; j < train_data->fea_matrix[row].size(); ++j){
+                    idx = train_data->fea_matrix[row][j].fid;
+                    wx += weight[idx];
                 }
                 pctr = sigmoid(wx);
                 float delta = pctr - train_data->label[row];
                 for(int j = 0; j < keys.size(); j++){
-                    g[keys[j]] += delta * values[j];
+                    iter = gradient.find(keys[j]);
+                    if(iter == gradient.end()){
+                        float g = iter->second + delta;
+                        gradient.insert(std::pair<size_t, float>(keys[j], g));
+                    }
+                    else{
+                        float g = iter->second + delta;
+                        gradient[keys[j]] = delta;
+                    }
                 }
             }
-            std::vector<ps::Key> nonzero_index;
-            std::vector<float> nonzero_gradient;
-            filter_zero_element(g, nonzero_index, nonzero_gradient);
-            kv_.Wait(kv_.Push(nonzero_index, nonzero_gradient));//put gradient to servers;
+
+            std::vector<ps::Key> push_keys;
+            std::vector<float> push_gradient;
+            for(iter = weight.begin(); iter != weight.end(); ++iter){
+                push_keys.push_back(iter->first);
+                push_gradient.push_back(gradient[iter->first]);
+            }
+
+            kv_.Wait(kv_.Push(push_keys, push_gradient));//put gradient to servers;
+
             mutex.lock();
             calculate_gradient_thread_count++;
             mutex.unlock();
@@ -138,13 +165,11 @@ class Worker : public ps::App{
                     break;
                 }
                 calculate_gradient_thread_count = 0;
-                std::vector<float> w_all;
-                kv_.Wait(kv_.Pull(init_index, &w_all));//get weight from servers
 
                 for(int i = 0; i < core_num; ++i){
                     start = i * thread_batch;
                     end = (i + 1) * thread_batch;
-                    pool.enqueue(std::bind(&Worker::calculate_batch_gradient, this, start, end, w_all));
+                    pool.enqueue(std::bind(&Worker::calculate_batch_gradient, this, start, end));
                 }//end for
                 while(calculate_gradient_thread_count < core_num);
                 if((batch + 1) % 20 == 0)std::cout<<"rank "<<rank<<" batch = "<<batch<<std::endl;
@@ -166,20 +191,20 @@ class Worker : public ps::App{
             ThreadPool pool(core_num);
 
             batch_num = train_data->fea_matrix.size() / batch_size;
+            std::cout<<"batch_num : "<<batch_num<<std::endl;
+
             for(int epoch = 0; epoch < epochs; ++epoch){
-                std::cout<<"epoch "<<epoch<<" ";
                 for(int i = 0; i < batch_num; ++i){
+                    if((batch_num + 1)%30 == 0) std::cout<<"rank "<<rank<<" epoch "<<epoch<<" batch "<<i<<std::endl;
                     int all_start = i * batch_size;
                     int thread_batch = batch_size / core_num;
                     int start, end;
                     calculate_gradient_thread_count = 0;
-                    std::vector<float> w_all;
-                    kv_.Wait(kv_.Pull(init_index, &w_all));//get weight from servers
 
                     for(int j = 0; j < core_num; ++j){
                         start = all_start + j * thread_batch;
                         end = all_start + (j + 1) * thread_batch;
-                        pool.enqueue(std::bind(&Worker::calculate_batch_gradient, this, start, end, w_all));
+                        pool.enqueue(std::bind(&Worker::calculate_batch_gradient, this, start, end));
                     }
                     while(calculate_gradient_thread_count < core_num);//m
                 }
@@ -195,18 +220,14 @@ class Worker : public ps::App{
             rank = ps::MyRank();
             snprintf(train_data_path, 1024, "%s-%05d", train_file_path, rank);
 
-            init_index.clear();
-            for(int i = 0; i < 2e6; i++){
-                init_index.push_back(i);
-            }
-            std::vector<float> init_val(2e6, 0.0);
-            kv_.Wait(kv_.Push(init_index, init_val));
-           
             core_num = std::thread::hardware_concurrency();
 
-            batch_learning(core_num);
-            return;
-            online_learning(core_num);
+            if(is_online_learning == true){
+                batch_learning(core_num);
+            }
+            else if(is_batch_learning == true){
+                online_learning(core_num);
+            }
         }//end process
 
     public:
@@ -215,6 +236,8 @@ class Worker : public ps::App{
         int batch_size = 400;
         int epochs = 100;
         int calculate_gradient_thread_count;
+        bool is_online_learning = false;
+        bool is_batch_learning = true;
 
         std::mutex mutex;
         std::vector<ps::Key> init_index;
