6e914e232a393e3b662eb56a1d298a5d20f22445
wangxiaoshu <wangxiaoshu@yidian-inc.com>
2016-12-29 11:19:28 +0800

    rm: auc_evaluation/

--- auc_evaluation/data/run_split_data.sh
+++
@@ -1,5 +0,0 @@
-rm input_data-0000*
-python split_data.py $1 3 input_data
-scp input_data-0000* slave1:/home/worker/xiaoshu/AUC-caculate-mpi/data
-scp input_data-0000* slave2:/home/worker/xiaoshu/AUC-caculate-mpi/data
-
