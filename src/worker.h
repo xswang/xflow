07418f7d29c739d2fe280c332ebb48217a56fbea
wangxiaoshu <wangxiaoshu@yidian-inc.com>
2016-12-30 15:35:59 +0800

    update: refactor

--- src/worker.h
+++ src/worker.h
@@ -59,94 +59,10 @@ class Worker : public ps::App{
 	    Process();
 	}
 
-	void calculate_pctr(int start, int end){
-	        auto all_keys = std::vector<sample_key>();
-                auto unique_keys = std::make_shared<std::vector<ps::Key>>();
-                int line_num = 0;
-                for(int row = start; row < end; ++row) {
-                    int sample_size = test_data->fea_matrix[row].size();
-                    sample_key sk;
-                    sk.sid = line_num;
-                    for(int j = 0; j < sample_size; ++j) {
-                        size_t idx = h(test_data->fea_matrix[row][j].fid);
-                        sk.fid = idx;
-                        all_keys.push_back(sk);
-                        (*unique_keys).push_back(idx);
-                    }
-                    ++line_num;
-                }
-                std::sort(all_keys.begin(), all_keys.end(), Worker::sort_finder);
-                std::sort((*unique_keys).begin(), (*unique_keys).end());
-                (*unique_keys).erase(unique((*unique_keys).begin(), (*unique_keys).end()), (*unique_keys).end());
-                auto w = std::make_shared<std::vector<float>>();
-                int keys_size = (*unique_keys).size();
-                kv_.Wait(kv_.ZPull(unique_keys, &(*w)));
-                auto wx = std::vector<float>(line_num);
-
-                for(int j = 0, i = 0; j < all_keys.size();){
-                    size_t allkeys_fid = all_keys[j].fid;
-                    size_t weight_fid = (*unique_keys)[i];
-                    if(allkeys_fid == weight_fid){
-                        wx[all_keys[j].sid] += (*w)[i];
-                        ++j;
-                    }
-                    else if(allkeys_fid > weight_fid){
-                        ++i;
-                    }
-                }
-                for(int i = 0; i < wx.size(); ++i){
-                    float pctr = sigmoid(wx[i]);
-                    auc_key ak;
-                    ak.label = test_data->label[start++];
-                    ak.pctr = pctr;
-		    mutex.lock();
-                    test_auc_vec.push_back(ak);
-		    mutex.unlock();
-                    //md<<pctr<<"\t"<<1 - test_data->label[i]<<"\t"<<test_data->label[i]<<std::endl;
-                }
-		--calculate_pctr_thread_finish_num;
-	}
-
-        void predict(ThreadPool &pool, int rank){
-            char buffer[1024];
-            snprintf(buffer, 1024, "%d", rank);
-            std::string filename = buffer;
-            std::ofstream md;
-            md.open("pred_" + filename + ".txt");
-            if(!md.is_open()) std::cout<<"open pred file failure!"<<std::endl;
-
-	    snprintf(test_data_path, 1024, "%s-%05d", test_file_path, rank);
-	    test_data = new dml::LoadData(test_data_path, ((size_t)4)<<30);
-            std::cout<<"alloc 4GB memory sucess!"<<std::endl;
-	    test_auc_vec.clear();
-	    while(true){
-		test_data->load_minibatch_hash_data_fread();
-		std::cout<<"test_data size = "<<test_data->fea_matrix.size()<<std::endl;
-	        if(test_data->fea_matrix.size() <= core_num * 5) break;
-		int thread_size = train_data->fea_matrix.size() / core_num;
-		calculate_pctr_thread_finish_num = core_num;
-                for(int i = 0; i < core_num; ++i){
-                    int start = i * thread_size;
-                    int end = (i + 1)* thread_size;
-                    pool.enqueue(std::bind(&Worker::calculate_pctr, this, start, end));
-                }//end all batch
-		while(calculate_pctr_thread_finish_num > 0) usleep(10);
-            }//end while
-	    delete test_data;
-            md.close();
-	    calculate_auc(test_auc_vec);
-        }//end predict 
-
-        inline void filter_zero_element(std::vector<float>& gradient, std::vector<ps::Key>& nonzero_index, std::vector<float>& nonzero_gradient){
-            for(int i = 0; i < init_index.size(); i++){
-                int idx = init_index[i];
-                float g = gradient[idx];
-                if(g != 0.0){
-                    nonzero_index.push_back(idx);
-                    nonzero_gradient.push_back(g);
-                }
-            }
-        }
+        struct auc_key{
+            int label;
+            float pctr;
+        };
 
         timespec time_diff(timespec start, timespec end){
             timespec tmp;
@@ -168,11 +84,107 @@ class Worker : public ps::App{
             return a.fid == b.fid;
         }
 
+        void calculate_auc(std::vector<auc_key>& auc_vec){
+            std::sort(auc_vec.begin(), auc_vec.end(), [](const auc_key& a, const auc_key& b){
+                return a.pctr > b.pctr;
+            });
+            float area = 0.0; 
+	    int tp_n = 0;
+            for(size_t i = 0; i < auc_vec.size(); ++i){
+                if(auc_vec[i].label > 0) tp_n += 1;
+                else area += tp_n;
+            }
+            if (tp_n == 0 || tp_n == auc_vec.size()) std::cout<<"tp_n = "<<tp_n<<std::endl;
+            else{
+                area /= (tp_n * (auc_vec.size() - tp_n));
+                std::cout<<"auc = "<<area<<std::endl;
+            }
+            auc_vec.clear();
+        }
+
+	void calculate_pctr(int start, int end){
+	    auto all_keys = std::vector<sample_key>();
+	    auto unique_keys = std::make_shared<std::vector<ps::Key>>();
+	    int line_num = 0;
+	    for(int row = start; row < end; ++row) {
+		int sample_size = test_data->fea_matrix[row].size();
+		sample_key sk;
+		sk.sid = line_num;
+		for(int j = 0; j < sample_size; ++j) {
+		    size_t idx = test_data->fea_matrix[row][j].fid;
+		    sk.fid = idx;
+		    all_keys.push_back(sk);
+		    (*unique_keys).push_back(idx);
+		}
+		++line_num;
+            }
+            std::sort(all_keys.begin(), all_keys.end(), Worker::sort_finder);
+	    std::sort((*unique_keys).begin(), (*unique_keys).end());
+	    (*unique_keys).erase(unique((*unique_keys).begin(), (*unique_keys).end()), (*unique_keys).end());
+	    auto w = std::make_shared<std::vector<float>>();
+	    int keys_size = (*unique_keys).size();
+	    kv_.Wait(kv_.ZPull(unique_keys, &(*w)));
+	    auto wx = std::vector<float>(line_num);
+
+            for(int j = 0, i = 0; j < all_keys.size();){
+                size_t allkeys_fid = all_keys[j].fid;
+                size_t weight_fid = (*unique_keys)[i];
+                if(allkeys_fid == weight_fid){
+                    wx[all_keys[j].sid] += (*w)[i];
+                    ++j;
+                }
+                else if(allkeys_fid > weight_fid){
+                    ++i;
+                }
+            }
+            for(int i = 0; i < wx.size(); ++i){
+                float pctr = sigmoid(-1.0 * wx[i]);
+                auc_key ak;
+                ak.label = test_data->label[start++];
+                ak.pctr = pctr;
+		mutex.lock();
+                test_auc_vec.push_back(ak);
+		mutex.unlock();
+                //md<<pctr<<"\t"<<1 - test_data->label[i]<<"\t"<<test_data->label[i]<<std::endl;
+            }
+	    --calculate_pctr_thread_finish_num;
+	}//calculate_pctr
+
+        void predict(ThreadPool &pool, int rank){
+	    char buffer[1024];
+            snprintf(buffer, 1024, "%d", rank);
+            std::string filename = buffer;
+            std::ofstream md;
+            md.open("pred_" + filename + ".txt");
+            if(!md.is_open()) std::cout<<"open pred file failure!"<<std::endl;
+
+	    snprintf(test_data_path, 1024, "%s-%05d", test_file_path, rank);
+	    test_data = new dml::LoadData(test_data_path, ((size_t)4)<<30);
+            std::cout<<"alloc 4GB memory sucess!"<<std::endl;
+	    test_auc_vec.clear();
+	    while(true){
+		test_data->load_minibatch_hash_data_fread();
+		std::cout<<"test_data size = "<<test_data->fea_matrix.size()<<std::endl;
+	        if(test_data->fea_matrix.size() <= 0) break;
+		int thread_size = train_data->fea_matrix.size() / core_num;
+		calculate_pctr_thread_finish_num = core_num;
+                for(int i = 0; i < core_num; ++i){
+                    int start = i * thread_size;
+                    int end = (i + 1)* thread_size;
+                    pool.enqueue(std::bind(&Worker::calculate_pctr, this, start, end));
+                }//end all batch
+		while(calculate_pctr_thread_finish_num > 0) usleep(10);
+            }//end while
+	    delete test_data;
+	    calculate_auc(test_auc_vec);
+            md.close();
+        }//end predict 
+
         void zpush_callback(std::vector<sample_key> all_keys, std::shared_ptr<std::vector<ps::Key>> unique_keys, std::shared_ptr<std::vector<float>> w, int start, int end){
             auto wx = std::vector<float>(end - start + 1);
             for(int j = 0, i = 0; j < all_keys.size();){
-                int allkeys_fid = all_keys[j].fid;
-                int weight_fid = (*unique_keys)[i];
+                size_t allkeys_fid = all_keys[j].fid;
+                size_t weight_fid = (*unique_keys)[i];
                 if(allkeys_fid == weight_fid){
                     wx[all_keys[j].sid] += (*w)[i];
                     ++j;
@@ -183,15 +195,15 @@ class Worker : public ps::App{
             }
 
             for(int i = 0; i < wx.size(); i++){
-                float pctr = sigmoid(wx[i]);
+                float pctr = sigmoid(-1.0 * wx[i]);
                 float loss = pctr - train_data->label[start++];
                 wx[i] = loss;
             }
 
             auto push_gradient = std::make_shared<std::vector<float> > ((*unique_keys).size());
             for(int j = 0, i = 0; j < all_keys.size();){
-                int allkeys_fid = all_keys[j].fid;
-                int gradient_fid = (*unique_keys)[i];
+                size_t allkeys_fid = all_keys[j].fid;
+                size_t gradient_fid = (*unique_keys)[i];
                 int sid = all_keys[j].sid;
                 if(allkeys_fid == gradient_fid){
                     (*push_gradient)[i] += wx[sid];
@@ -239,13 +251,6 @@ class Worker : public ps::App{
         }//calculate_batch_gradient_callback
 
         void batch_learning_callback(int core_num){
-            /*
-            train_data_kafka = new dml::LoadData_from_kafka();
-            std::cout<<"start load kafka data"<<std::endl;
-            train_data_kafka->load_data_from_kafka(10);
-            std::cout<<"load kafka data end"<<std::endl;
-            return;
-            */
             train_data = new dml::LoadData(train_data_path, 1);
             train_data->load_all_data();
             ThreadPool pool(core_num);
@@ -274,11 +279,6 @@ class Worker : public ps::App{
                 std::cout<<"rank "<<rank<<" send_key_number avage: "<<send_key_numbers * 1.0 / (batch_num * core_num)<<std::endl;
             }//end all epoch
         }//end batch_learning_callback
-
-        struct auc_key{
-	    int label;
-	    float pctr;
-	};
 
         void calculate_batch_gradient_threadpool(int start, int end){
             timespec all_start, all_end, all_elapsed_time;
@@ -324,19 +324,9 @@ class Worker : public ps::App{
                 }
             }//end for
             for(int i = 0; i < wx.size(); i++){
-                pctr = sigmoid(wx[i]);
+                pctr = sigmoid(-1.0 * wx[i]);
 		int l = train_data->label[start];
                 float loss = pctr - train_data->label[start++];
-		/*
-                mutex.lock();
-		logloss += l*log(pctr) + (1-l)*log(1-pctr); 
-	        rmse += loss * loss;
-		auc_key ak;
-                ak.label = l;
-		ak.pctr = pctr;
-		auc_vec.push_back(ak); 
-		mutex.unlock();
-		*/
                 wx[i] = loss;
             }
 
@@ -369,30 +359,11 @@ class Worker : public ps::App{
 	    --calculate_batch_gradient_thread_finish_num;
         }
 
-	void calculate_auc(std::vector<auc_key>& auc_vec){
-            std::sort(auc_vec.begin(), auc_vec.end(), [](const auc_key& a, const auc_key& b){
-	   	return a.pctr < b.pctr;
-	    });
-	    float area = 0.0; int tp_n = 0;
-	    for(size_t i = 0; i < auc_vec.size(); ++i){
-		if(auc_vec[i].label > 0) tp_n += 1;
-		else area += tp_n;
-	    }
-	    if (tp_n == 0 || tp_n == auc_vec.size()) std::cout<<"tp_n = "<<tp_n<<std::endl;
-	    else{
-		area /= (tp_n * (auc_vec.size() - tp_n));
-		std::cout<<"auc = "<<area<<std::endl;
-		auc_vec.clear();
-	    }
-	}
-
         void batch_learning_threadpool(int core_num){
             ThreadPool pool(core_num);
             timespec allstart, allend, allelapsed;
             for(int epoch = 0; epoch < epochs; ++epoch){
                 train_data = new dml::LoadData(train_data_path, block_size<<20);
-                clock_gettime(CLOCK_MONOTONIC, &allstart);
-                send_key_numbers = 0;
 		int block = 0;
                 while(1){
                     train_data->load_minibatch_hash_data_fread();
@@ -407,26 +378,15 @@ class Worker : public ps::App{
 	 	    while(calculate_batch_gradient_thread_finish_num > 0){
 			usleep(10);
 		    }
-		    if((block + 1) % 50 == 0){
-			//calculate_auc(auc_vec);
-                        //std::cout<<"rank "<<rank<<" logloss = "<<logloss * -1.0 /train_data->fea_matrix.size()<<"\trmse = "<<sqrt(rmse) * 1.0 / train_data->fea_matrix.size()<<std::endl;
-                        logloss = 0.0;
-			rmse = 0.0;
-		    	if(rank == 0) predict(pool, rank);
-		    }//end if(block)
 		    ++block;
-                }//end while
-                clock_gettime(CLOCK_MONOTONIC, &allend);
-                allelapsed = time_diff(allstart, allend);
-                std::cout<<"rank "<<rank<<" per process : "<<train_data->fea_matrix.size() * 1e9 * 1.0 / (allelapsed.tv_sec * 1e9 + allelapsed.tv_nsec)<<std::endl;
-                std::cout<<"rank "<<rank<<" send_key_number avage: "<<send_key_numbers * 1.0 / (batch_num * core_num)<<std::endl;
+                }//end while one epoch
 		delete train_data;
+		if(rank == 0) predict(pool, rank);
             }//end for all epoch
         }//end batch_learning_threadpool
 
         virtual void Process(){
             rank = ps::MyRank();
-            
             snprintf(train_data_path, 1024, "%s-%05d", train_file_path, rank);
             core_num = std::thread::hardware_concurrency();
 	    std::cout<<"core_num = "<<core_num<<std::endl;
