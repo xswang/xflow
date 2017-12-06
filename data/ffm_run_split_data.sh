af49b2ea01d870a9e22c1adaf8d4f5249f05eb55
xswang <2012wxs@gmail.com>
2017-12-07 07:49:57 +0800

    rm

--- data/ffm_run_split_data.sh
+++
@@ -1,9 +0,0 @@
-#rm ffmdata_test-0000*
-#python split_data.py ffmdata_test.txt 3 ffmdata_test
-scp ff_test-0000* slave1:/home/worker/xiaoshu/Field-aware-Factorization-Machine-mpi/data
-scp ff_test-0000* slave2:/home/worker/xiaoshu/Field-aware-Factorization-Machine-mpi/data
-
-#rm ffmdata_train-0000*
-#python split_data.py ffmdata_train.txt 3 ffmdata_train
-scp ff_train-0000* slave1:/home/worker/xiaoshu/Field-aware-Factorization-Machine-mpi/data
-scp ff_train-0000* slave2:/home/worker/xiaoshu/Field-aware-Factorization-Machine-mpi/data
