418821f4c7263c101cb4ea6711d954cb9286ea61
xswang <2012wxs@gmail.com>
2017-12-09 20:37:13 +0800

    update: demo for 1 server 3 workers run on local

--- scripts/run_ps_local.sh
+++ scripts/run_ps_local.sh
@@ -1,2 +1,2 @@
-sh local.sh 1 3 /Users/xiaoshuwang/documents/oneflow/opensource/logistic_regression_ps/bin/lr_ps ~/documents/data/transform/train_new.ffm ~/documents/data/transform/test_new.ffm
-#sh local.sh 1 3 /Users/xiaoshuwang/documents/oneflow/opensource/logistic_regression_ps/bin/lr_ps /Users/xiaoshuwang/documents/oneflow/opensource/logistic_regression_ps/data/lr_train /Users/xiaoshuwang/documents/oneflow/opensource/logistic_regression_ps/data/lr_test
+#sh local.sh 1 3 /Users/xiaoshuwang/documents/oneflow/opensource/logistic_regression_ps/bin/lr_ps ~/documents/data/transform/train_new.ffm ~/documents/data/transform/test_new.ffm
+sh local.sh 1 3 ~/documents/oneflow/opensource/logistic_regression_ps/bin/lr_ps ~/documents/oneflow/opensource/logistic_regression_ps/data/small_train ~/documents/oneflow/opensource/logistic_regression_ps/data/small_test
