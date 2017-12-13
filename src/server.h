52f2a936d86d3f461faa65b8b73e36ef8d2aa029
xswang <2012wxs@gmail.com>
2017-12-13 18:08:16 +0800

    update: w

--- src/server.h
+++ src/server.h
@@ -7,17 +7,20 @@ float beta = 1.0;
 float lambda1 = 5e-5;
 float lambda2 = 15.0;
 
-struct FTRLEntry{
-  FTRLEntry() : w(0.0), z(0.0), n(0.0) {
-
+typedef struct FTRLEntry{
+  FTRLEntry(int k = 10) {
+    w.resize(k, 0.0);
+    n.resize(k, 0.0);
+    z.resize(k, 0.0);
   }
-  float w;
-  float z;
-  float n;
-};
+  std::vector<float> w;
+  std::vector<float> n;
+  std::vector<float> z;
+} ftrlentry;
 
 struct KVServerFTRLHandle {
   void operator()(const ps::KVMeta& req_meta, const ps::KVPairs<float>& req_data, ps::KVServer<float>* server) {
+    int k = 1;
     size_t n = req_data.keys.size();
     ps::KVPairs<float> res;
     if (req_meta.push) {
@@ -29,29 +32,34 @@ struct KVServerFTRLHandle {
     for (size_t i = 0; i < n; ++i) {
       ps::Key key = req_data.keys[i];
       FTRLEntry& val = store[key];
-      if (req_meta.push) {
-        float g = req_data.vals[i];
-        float old_n = val.n;
-        float n = old_n + g * g;
-        val.z += g - (std::sqrt(n) - std::sqrt(old_n)) / alpha * val.w;
-        val.n = n;
-        if (std::abs(val.z) <= lambda1) {
-          val.w = 0.0;
+      for (int j = 0; j < 1; ++j){
+        if (req_meta.push) {
+          float g = req_data.vals[i * k + j];
+          float old_n = val.n[j];
+          float n = old_n + g * g;
+          val.z[j] += g - (std::sqrt(n) - std::sqrt(old_n)) / alpha * val.w[j];
+          val.n[j] = n;
+          if (std::abs(val.z[j]) <= lambda1) {
+            val.w[j] = 0.0;
+          } else {
+            float tmpr= 0.0;
+            if (val.z[j] > 0.0) tmpr = val.z[j] - lambda1;
+            if (val.z[j] < 0.0) tmpr = val.z[j] + lambda1;
+            float tmpl = -1 * ( (beta + std::sqrt(val.n[j]))/alpha  + lambda2 );
+            val.w[j] = tmpr / tmpl;
+          }
         } else {
-          float tmpr= 0.0;
-          if (val.z > 0.0) tmpr = val.z - lambda1;
-          if (val.z < 0.0) tmpr = val.z + lambda1;
-          float tmpl = -1 * ( (beta + std::sqrt(val.n))/alpha  + lambda2 );
-          val.w = tmpr / tmpl;
+          for (int j = 0; j < 1; ++j) {
+            res.vals[i * k + j] = val.w[j];
+          }
         }
-      } else {
-        res.vals[i] = val.w;
       }
     }
     server->Response(req_meta, res);
   }
  private:
-  std::unordered_map<ps::Key, FTRLEntry> store;
+  //std::unordered_map<ps::Key, FTRLEntry> store;
+  std::unordered_map<ps::Key, ftrlentry> store;
 };
 
 class S{
