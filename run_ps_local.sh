e1819a2cbc786b5add1d9e72b81fdb17f7808f08
xswang <2012wxs@gmail.com>
2018-01-19 19:12:27 +0800

    update: run_ps_local

--- run_ps_local.sh
+++ run_ps_local.sh
@@ -1,2 +1,3 @@
-#sh local.sh 1 3 /Users/xiaoshuwang/documents/oneflow/opensource/logistic_regression_ps/bin/lr_ps ~/documents/data/transform/train_new.ffm ~/documents/data/transform/test_new.ffm
+path=`pwd`
+echo $path
 sh local.sh 1 3 ~/documents/oneflow/opensource/logistic_regression_ps/bin/lr_ps ~/documents/oneflow/opensource/logistic_regression_ps/data/small_train ~/documents/oneflow/opensource/logistic_regression_ps/data/small_test
