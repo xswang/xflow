704a663aae0ce24f2c62a64a561f835f4ac32d0f
xswang <2012wxs@gmail.com>
2018-01-19 19:54:48 +0800

    update: main.cc

--- src/main.cc
+++ src/main.cc
@@ -4,17 +4,18 @@
 #include "src/server.h"
 
 #include "ps/ps.h"
-
+namespace xflow{
 int v_dim;
+}
 
 int main(int argc,char *argv[]){
-  v_dim = 1;
+  xflow::v_dim = 1;
   if (ps::IsServer()) {
-    S* server = new S();
+    xflow::Server* server = new xflow::Server();
   }
   ps::Start();
   if (ps::IsWorker()) {
-    LRWorker* lr_worker = new LRWorker(argv[1], argv[2]);
+    xflow::LRWorker* lr_worker = new xflow::LRWorker(argv[1], argv[2]);
     lr_worker->train();
   }
   ps::Finalize();
