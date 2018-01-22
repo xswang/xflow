ae0a27005c6061e8bb9c11e0270563930d1f71d7
xswang <2012wxs@gmail.com>
2018-01-22 21:44:01 +0800

    update: stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,2 @@
-ps -ef | grep lr_ps | awk '{ print $2 }' | sudo xargs kill -9
+ps -ef | grep xflow_lr | awk '{ print $2 }' | sudo xargs kill -9
 #ps -ef | grep dump.sh | awk '{ print $2 }' | sudo xargs kill -9
