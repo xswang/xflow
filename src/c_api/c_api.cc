bfaa76383f0976efda52b76bc26433498ff85a7d
xswang <2012wxs@gmail.com>
2018-01-22 22:28:19 +0800

    update

--- src/c_api/c_api.cc
+++ src/c_api/c_api.cc
@@ -15,9 +15,8 @@ XFLOW_DLL int XFlowSetDataPath(void* *h,
                                const char* train_data_path,
                                const char* test_data_path) {
   XFlow* xf = reinterpret_cast<XFlow*>(*h);
-  xf->lr_worker_->train_file_path = train_data_path;
-  xf->lr_worker_->test_file_path = test_data_path;
-} 
+  xf->lr_worker_ = new xflow::LRWorker(train_data_path, test_data_path);
+}
 
 XFLOW_DLL int XFlowStartTrain(void* h) {
   XFlow* xf = reinterpret_cast<XFlow*>(*h);
