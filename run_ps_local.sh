22c99101d8a5d7a9b56607f1e9e135a13c81f534
xswang <2012wxs@gmail.com>
2018-01-26 18:06:23 +0800

    update: lr_worker.h

--- run_ps_local.sh
+++ run_ps_local.sh
@@ -3,3 +3,7 @@ echo $root_path
 model_name=$1
 epochs=$2
 sh ./scripts/local.sh 1 3 $root_path/build/test/src/xflow_lr $root_path/data/small_train $root_path/data/small_test $model_name $epochs
+
+# for test
+#data_path=/Users/xiaoshuwang/documents/data/libffm_toy
+#sh ./scripts/local.sh 1 3 $root_path/build/test/src/xflow_lr $data_path/criteo.tr.r100.gbdt0.ffm $data_path/criteo.va.r100.gbdt0.ffm $model_name $epochs
