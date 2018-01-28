8cb83ee35b108f427494e84770d3bff8ff77d550
xswang <2012wxs@gmail.com>
2018-01-28 11:49:30 +0800

    add: a new model. may be better than ffm and faster than ffm

---
+++ src/model/mvm/mvm_worker.h
@@ -0,0 +1,13 @@
+/*
+ * mvm.h
+ * Copyright (C) 2018 wangxiaoshu <2012wxs@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef MVM_H
+#define MVM_H
+
+
+
+#endif /* !MVM_H */
