38c8392aec96e733332bfedc41ed14ee49e623e7
xswang <2012wxs@gmail.com>
2018-01-30 16:50:46 +0800

    rm: linear from mvm

--- src/model/mvm/mvm_worker.h
+++ src/model/mvm/mvm_worker.h
@@ -14,17 +14,17 @@
 #include "src/io/load_data_from_disk.h"
 #include "src/base/thread_pool.h"
 #include "src/base/base.h"
-
 #include "ps/ps.h"
 
 namespace xflow{
 class MVMWorker{
  public:
   MVMWorker(const char *train_file,
-    const char *test_file) :
-                           train_file_path(train_file),
-                           test_file_path(test_file) {
-    kv_w_ = new ps::KVWorker<float>(0);
+           const char *test_file) :
+           train_file_path(train_file),
+           test_file_path(test_file) {
+    kv_w = new ps::KVWorker<float>(0);
+    kv_v = new ps::KVWorker<float>(1);
     base_ = new Base;
     core_num = std::thread::hardware_concurrency();
     pool_ = new ThreadPool(core_num);
@@ -36,29 +36,33 @@ class MVMWorker{
 
   void calculate_gradient(std::vector<Base::sample_key>& all_keys,
                           std::vector<ps::Key>& unique_keys,
+                          size_t start, size_t end,
+                          std::vector<float>& v,
+                          std::vector<float>& v_sum,
                           std::vector<float>& loss,
-                          std::vector<float>& push_gradient);
-  void calculate_loss(std::vector<float>& w,
+                          std::vector<float>& push_v_gradient);
+  void calculate_loss(std::vector<float>& v,
                       std::vector<Base::sample_key>& all_keys,
                       std::vector<ps::Key>& unique_keys,
-                      size_t start,
-                      size_t end,
-                      std::vector<float>& loss); 
+                      size_t start, size_t end,
+                      std::vector<float>& v_sum,
+                      std::vector<float>& loss);
   void update(int start, int end);
   void batch_training(ThreadPool* pool);
   void train();
- 
+
  public:
   int epochs = 60;
-
  private:
   int rank;
   int core_num;
   int block_size = 2;
 
+  std::atomic_llong num_batch_fly = {0};
   std::atomic_llong gradient_thread_finish_num = {0};
   std::atomic_llong calculate_pctr_thread_finish_num = {0};
 
+  std::vector<Base::auc_key> auc_vec;
   std::vector<Base::auc_key> test_auc_vec;
 
   std::ofstream md;
@@ -71,6 +75,8 @@ class MVMWorker{
   const char *test_file_path;
   char train_data_path[1024];
   char test_data_path[1024];
-  ps::KVWorker<float>* kv_w_;
-};
+  int v_dim_ = 10;
+  ps::KVWorker<float>* kv_w;
+  ps::KVWorker<float>* kv_v;
+};//end class worker
 }
