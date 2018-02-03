cf04b1ee546d0a697fcb296cf27904f91e95079c
xswang <2012wxs@gmail.com>
2018-02-03 08:41:11 +0800

    google code style

--- src/base/thread_pool.h
+++ src/base/thread_pool.h
@@ -1,6 +1,14 @@
-#ifndef THREAD_POOL_H
-#define THREAD_POOL_H
+/*
+ * base.h
+ * Copyright (C) 2018 wangxiaoshu <2012wxs@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
 
+#ifndef SRC_BASE_THREAD_POOL_H_
+#define SRC_BASE_THREAD_POOL_H_
+
+#include <unistd.h>
 #include <vector>
 #include <queue>
 #include <memory>
@@ -11,22 +19,21 @@
 #include <functional>
 #include <stdexcept>
 #include <atomic>
-#include <unistd.h>
 
-namespace xflow{
+namespace xflow {
 class ThreadPool {
-public:
-    ThreadPool(size_t);
+ public:
+    explicit ThreadPool(size_t);
     template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) 
+    auto enqueue(F&& f, Args&&... args)
         -> std::future<typename std::result_of<F(Args...)>::type>;
     ~ThreadPool();
-private:
+ private:
     // need to keep track of threads so we can join them
     std::vector< std::thread > workers;
     // the task queue
     std::queue< std::function<void()> > tasks;
-    
+
     std::atomic_llong tasks_number = {0};
 
     int queue_max_threads_num = 64;
@@ -35,72 +42,58 @@ private:
     std::condition_variable condition;
     bool stop;
 };
- 
+
 // the constructor just launches some amount of workers
 inline ThreadPool::ThreadPool(size_t threads)
-    :   stop(false)
-{
-    for(size_t i = 0;i<threads;++i)
-        workers.emplace_back(
-            [this]
-            {
-                for(;;)
-                {
-                    std::function<void()> task;
-                    {
-                        std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
-                        if(this->stop && this->tasks.empty())
-                            return;
-                        task = std::move(this->tasks.front());
-                        this->tasks.pop();
-                    }
-                    tasks_number -= 1;
-                    task();
-                }
-            }
-        );
+    :   stop(false) {
+  for (size_t i = 0; i < threads; ++i)
+    workers.emplace_back(
+      [this] {
+        for (;;) {
+          std::function<void()> task;
+          {
+            std::unique_lock<std::mutex> lock(this->queue_mutex);
+            this->condition.wait(lock,
+              [this] { return this->stop || !this->tasks.empty(); });
+            if (this->stop && this->tasks.empty())
+              return;
+            task = std::move(this->tasks.front());
+            this->tasks.pop();
+          }
+          tasks_number -= 1;
+           task();
+        }
+      });
 }
 
 // add new work item to the pool
 template<class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>
-{
-    using return_type = typename std::result_of<F(Args...)>::type;
-
-    auto task = std::make_shared< std::packaged_task<return_type()> >(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
-        
-    std::future<return_type> res = task->get_future();
-    while(tasks_number >= queue_max_threads_num) usleep(1); 
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        // don't allow enqueueing after stopping the pool
-        if(stop) throw std::runtime_error("enqueue on stopped ThreadPool");
-
-        tasks.emplace([task](){ (*task)(); });
-        //std::cout<<"tasks size "<<tasks.size()<<std::endl;
-        //std::cout<<"tasks number "<<tasks_number<<std::endl;
-    }
-    tasks_number += 1;
-    condition.notify_one();
-    return res;
+auto ThreadPool::enqueue(F&& f, Args&&... args)
+    -> std::future<typename std::result_of<F(Args...)>::type> {
+  using return_type = typename std::result_of<F(Args...)>::type;
+  auto task = std::make_shared< std::packaged_task<return_type()> >(
+              std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+  std::future<return_type> res = task->get_future();
+  while (tasks_number >= queue_max_threads_num) usleep(1);
+  {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    if (stop) throw std::runtime_error("enqueue on stopped ThreadPool");
+    tasks.emplace([task](){ (*task)(); });
+  }
+  tasks_number += 1;
+  condition.notify_one();
+  return res;
 }
 
 // the destructor joins all threads
-inline ThreadPool::~ThreadPool()
-{
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        stop = true;
-    }
-    condition.notify_all();
-    for(std::thread &worker: workers)
-        worker.join();
+inline ThreadPool::~ThreadPool() {
+  {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    stop = true;
+  }
+  condition.notify_all();
+  for (std::thread &worker : workers)
+    worker.join();
 }
-}
-#endif
-
+}  // namespace xflow
+#endif  // SRC_BASE_THREAD_POOL_H_
